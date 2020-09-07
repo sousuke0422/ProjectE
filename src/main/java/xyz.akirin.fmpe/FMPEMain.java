5eada5bf0669490e93b3c088030e1cb0abdab56b
sousuke0422 <sousuke20xx@gmail.com>
2020-09-07 21:17:12 +0900

    FMPE

---
+++ src/main/java/xyz.akirin.fmpe/FMPEMain.java
@@ -0,0 +1,10 @@
+package xyz.akirin.fmpe;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = FMPEMain.ID, name = FMPEMain.Name, version = FMPEMain.Version)
+public class FMPEMain {
+    static final String ID = "FMPE";
+    static final String Name = "FMProjectE";
+    static final String Version = "core:1.0.0, codename:none";
+}
